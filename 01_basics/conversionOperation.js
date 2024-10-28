bddf562fda0334807a04b5ef9424b20c543da4b9
Ashwani <119045615+ashuu02103@users.noreply.github.com>
2024-10-28 17:01:28 +0000

    conversion of data type

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "hitesh"
+
+//console.log(typeof score);          We can write both types.
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (None a Number)
+//  true => 1; false => 0
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
