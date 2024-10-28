1d99fbcd60a5729d49cb54ce90e4033ab9dc1094
Ashwani <119045615+ashuu02103@users.noreply.github.com>
2024-10-28 16:02:22 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code in newer version 
+// we are using node.js,  not browser
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
